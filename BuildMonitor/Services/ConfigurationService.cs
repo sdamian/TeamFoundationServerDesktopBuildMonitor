108d788910f866abcb9b347f196f78df55fe8d57
Jason Finch <jason.finch@gmail.com>
2013-04-04 14:39:27 +1000

    Basis for moving config from registry to config file.

---
+++ BuildMonitor/Services/ConfigurationService.cs
@@ -0,0 +1,12 @@
+﻿using JsonConfig;
+
+namespace BuildMonitor.Services
+{
+    public class ConfigurationService
+    {
+         public void ReadConfig()
+         {
+             Config
+         }
+    }
+}
